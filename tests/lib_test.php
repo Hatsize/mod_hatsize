4943fcaa5f80f07c476f7906ddbc0ddf4933583e
rrepucci2 <rrepucci@hatsize.com>
2016-07-01 08:34:56 -0400

    Update lib_test.php

--- tests/lib_test.php
+++ tests/lib_test.php
@@ -19,7 +19,6 @@
  * @copyright   2016 Hatsize Learning {@link http://hatsize.com}
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- */
 
 defined('MOODLE_INTERNAL') || die();
 
